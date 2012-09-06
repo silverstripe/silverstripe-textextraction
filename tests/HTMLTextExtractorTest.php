788a49bf9f5422a1d8a5f415e506f70efe0ea0ec
Ingo Schommer <ingo@silverstripe.com>
2012-09-06 13:41:21 +0200

    BUG Improved HTMLTextExtractor, remove non-content tags

--- tests/HTMLTextExtractorTest.php
+++ tests/HTMLTextExtractorTest.php
@@ -6,6 +6,9 @@ class HTMLTextExtractorTest extends SapphireTest {
 
 		$content = $extractor->getContent(Director::baseFolder() . '/textextraction/tests/fixtures/test1.html');
 		$this->assertContains('Test Headline', $content);
+		$this->assertNotContains('Test Comment', $content, 'Strips HTML comments');
+		$this->assertNotContains('Test Style', $content, 'Strips non-content style tags');
+		$this->assertNotContains('Test Script', $content, 'Strips non-content script tags');
 	}
 
 }
