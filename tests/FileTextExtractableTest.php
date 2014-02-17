526de4586ca9163beaa28ba97de4a5db577b4e2f
Sam Minnee <sam@silverstripe.com>
2014-02-18 10:41:49 +1300

    FIX: Fixed broken test caused by file being modified.

--- tests/FileTextExtractableTest.php
+++ tests/FileTextExtractableTest.php
@@ -6,17 +6,23 @@ class FileTextExtractableTest extends SapphireTest {
 	);
 
 	function testExtractFileAsText() {
+		// Create a copy of the file, as it may be clobbered by the test
+		// ($file->extractFileAsText() calls $file->write)
+		copy(BASE_PATH.'/textextraction/tests/fixtures/test1.html',BASE_PATH.'/textextraction/tests/fixtures/test1-copy.html');
+		
 		// Use HTML, since the extractor is always available
 		$file = new File(array(
-			'Name' => 'test1.html',
-			'Filename' => 'textextraction/tests/fixtures/test1.html'
+			'Name' => 'test1-copy.html',
+			'Filename' => 'textextraction/tests/fixtures/test1-copy.html'
 		));
-		// Don't write file, since it'd rename the file and make it inaccessible for subsequent tests
-
+		$file->write();
+	
 		$content = $file->extractFileAsText();
 		$this->assertContains('Test Headline', $content);
 		$this->assertContains('Test Text', $content);
 		$this->assertEquals($content, $file->FileContentCache);
+
+		if(file_exists(BASE_PATH.'/textextraction/tests/fixtures/test1-copy.html')) unlink(BASE_PATH.'/textextraction/tests/fixtures/test1-copy.html');
 	}
 
 
